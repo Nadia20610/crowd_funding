a2485233c49ddacb3ea6e2916c2e32117542e765
Nadia20610 <nadia.ahmed.mohamed20.com>
2025-04-07 16:27:52 +0200

    updated forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,24 +1,46 @@
 from django import forms
-from .models import Project, ProjectImage, Comment, Rating
-
+from django.core.exceptions import ValidationError
+from .models import Project, Comment, Rating , ProjectImage , Report
+from django.utils import timezone
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fildes = ['title', 'details', 'category', 'total_target', 'start_time', 'end_time', 'tags']
+        fields = ['title', 'details', 'category', 'tags', 'total_target', 'start_time', 'end_time']
+        widgets = {
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get('start_time')
+        end_time = cleaned_data.get('end_time')
+        
+        if start_time and end_time:
+            if start_time < timezone.now():
+                raise ValidationError("Start time cannot be in the past")
+            if end_time <= start_time:
+                raise ValidationError("End time must be after start time")
+        
+        return cleaned_data
 
-class ProjectImageForm(forms.ModelForm):
+class ProjectPictureForm(forms.ModelForm):
     class Meta:
         model = ProjectImage
-        fields = ['image']
+        fields = ['image', 'is_featured']
 
-class CommentForm(forms.ModelForm):
+class ProjectCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ['content']
 
-class RatingForm(forms.ModelForm):
+class ProjectRatingForm(forms.ModelForm):
     class Meta:
         model = Rating
-        fields = ['value']
+        fields = ['rating']
 
+class ProjectReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ['reason']
